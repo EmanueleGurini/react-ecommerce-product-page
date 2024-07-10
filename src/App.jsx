bd240c3f2a8c3b52f7bd4f22aacc1b60a1db013b
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-07-10 17:04:38 +0200

    feat: add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 //import styles from "./App.module.css";
 
+import Button from "./components/Button/Button";
+
 function App() {
   return (
     <>
       <h1>Ecommerce product page</h1>
+      <Button />
     </>
   );
 }
